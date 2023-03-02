113acb7d1f160b3c7b545984c3a801dafa19b00b
Laura <Lausnclu@hotmail.com>
2023-03-02 20:39:42 +0100

    feat: added verify token function

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const verifyToken = (req, res, next) => {
+    try {
+        const authorization = req.headers.authorization;
+        if (!authorization) {
+            return res.send('Invalid token');
+        }
+        const [strategy, token] = authorization.split(" ");
+        const decoded = jwt.verify(token, 'secreto');
+        req.userId = decoded.userId;
+        req.roleId = decoded.roleId;
+        // Necesitamos recuperar antes la info de authController.login
+        next();
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+module.exports = verifyToken;
